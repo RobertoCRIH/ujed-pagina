57073c190b7bc0557c01ffae89c61207598d604c
RobertoCRIH <108534863+RobertoCRIH@users.noreply.github.com>
2024-03-20 18:36:08 -0600

    arregle las fotos

--- src/pages/home/cards.jsx
+++ src/pages/home/cards.jsx
@@ -1,8 +1,6 @@
 import {motion} from "framer-motion";
 
-import imagen1 from "../../assets/images/classroom.jpg";
-import imagen2 from "../../assets/images/carreras.jpg";
-import imagen3 from "../../assets/images/posgrado.jpg";
+
 
 import Card from "./card";
 
@@ -19,13 +17,13 @@ function Cards() {
             
         >
             
-            <Card img={imagen1} title={"Media Superior"}
+            <Card img={"https://images.pexels.com/photos/356065/pexels-photo-356065.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"} title={"Media Superior"}
             desc={"Ofrecemos programas educativos de nivel medio superior que te brindan las herramientas necesarias para construir un camino hacia el éxito."}/>
             
-            <Card img={imagen2} title={"Carreras y licenciaturas"} 
+            <Card img={"https://images.pexels.com/photos/1181534/pexels-photo-1181534.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"} title={"Carreras y licenciaturas"} 
             desc={"Desde programas innovadores hasta oportunidades de aprendizaje práctico, estamos aquí para ayudarte a alcanzar tus metas profesionales."}/>
 
-            <Card img={imagen3} title={"Posgrados"}
+            <Card img={"https://images.pexels.com/photos/356040/pexels-photo-356040.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"} title={"Posgrados"}
             desc={"Da el siguiente paso en tu carrera con los posgrados de la UJED. Descubre programas de alto nivel que te preparan para destacar en tu campo profesional."}/>
 
         </div>
